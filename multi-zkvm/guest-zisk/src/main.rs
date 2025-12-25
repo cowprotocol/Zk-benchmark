578bce99977017a0fa8409815db6f1c11b1420b0
Architsharma7 <architsharma7911@gmail.com>
2025-12-26 03:47:27 +0530

    updated tiny-keccak for zisk to precompile

--- multi-zkvm/guest-zisk/src/main.rs
+++ multi-zkvm/guest-zisk/src/main.rs
@@ -21,27 +21,6 @@ fn keccak2(a: &Byte32, b: &Byte32) -> Byte32 {
     out
 }
 
-// NOTE: The problem with zisk:
-// The SDK’s stdin cap of 0x2000 bytes (~8 KB) makes it impractical to verify 64 signatures (8328 bytes).
-// Even when tried with 40 signatures, the server crashed with the error:
-// thread '<unnamed>' (1371833) panicked at /Users/runner/work/zisk/zisk/core/src/mem.rs:352:13:
-// Mem::read() section not found for addr: 84=54 with width: 8
-// thread '<unnamed>' panicked at /Users/archit/Zk-benchmark/multi-zkvm/guest/src/lib.rs:37:37:
-// bincode decode failed: UnexpectedEnd { additional: 1 }
-
-
-// this doesn't use the k256 patched version (0.14.0-pre.8) for zisk as
-// the version uses elliptic-curve = { version = "0.14.0-rc.7"} which results in errors while building.
-// error: the trait bound `R: crypto_bigint::rand_core::RngCore` is not satisfied
-//    --> /Users/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/elliptic-curve-0.14.0-rc.16/src/scalar/value.rs:69:40
-//     |
-// 69  |             inner: C::Uint::random_mod(rng, Self::MODULUS.as_nz_ref()),
-//     |                    ------------------- ^^^ the trait `DerefMut` is not implemented for `R`
-//     |                    |
-//     |                    required by a bound introduced by this call
-//     |
-//     = note: required for `R` to implement `crypto_bigint::rand_core::RngCore`
-// note: required by a bound in `random_mod`
 
 #[inline]
 fn schnorr_zisk(msg: &Byte32, ax: &Byte32, _ay: &Byte32, sig: &SchnorrSig) -> bool {
