dc144c3ce4973f6d7bea526f4362133a1532a1f7
Architsharma7 <architsharma7911@gmail.com>
2026-02-21 23:37:04 +0530

    added benchmarked plots for gnark gpu accelerated benchmarks

--- comb_auction/circuit/comb_gnark/prover/gpu_icicle.go
+++ comb_auction/circuit/comb_gnark/prover/gpu_icicle.go
@@ -3,18 +3,15 @@
 package main
 
 import (
-	"fmt"
-
+	ecc "github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend"
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/backend/witness"
 	"github.com/consensys/gnark/constraint"
-	cs_bn254 "github.com/consensys/gnark/constraint/bn254"
-
-	icicle_bn254 "github.com/consensys/gnark/backend/groth16/bn254/icicle"
 )
 
 func loadProvingKey(pkPath string) (groth16.ProvingKey, error) {
-	pk := new(icicle_bn254.ProvingKey)
+	pk := groth16.NewProvingKey(ecc.BN254)
 	if err := readFromFile(pkPath, pk); err != nil {
 		return nil, err
 	}
@@ -22,13 +19,7 @@ func loadProvingKey(pkPath string) (groth16.ProvingKey, error) {
 }
 
 func proveWithAccel(cs constraint.ConstraintSystem, pk groth16.ProvingKey, w witness.Witness) (groth16.Proof, error) {
-	r1cs, ok := cs.(*cs_bn254.R1CS)
-	if !ok {
-		return nil, fmt.Errorf("icicle build: expected *bn254.R1CS, got %T", cs)
-	}
-	icpk, ok := pk.(*icicle_bn254.ProvingKey)
-	if !ok {
-		return nil, fmt.Errorf("icicle build: expected *icicle_bn254.ProvingKey, got %T", pk)
-	}
-	return icicle_bn254.Prove(r1cs, icpk, w)
+	return groth16.Prove(cs, pk, w,
+		backend.WithIcicleAcceleration(),
+	)
 }
